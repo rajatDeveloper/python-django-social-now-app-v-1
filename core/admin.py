e59f2991c4cf0495695e2521db3e21e263fc4274
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2023-01-15 21:17:50 +0530

    done
    done

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Profile , Post , LikePost,FollowersCount
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(LikePost)
+admin.site.register(FollowersCount )
